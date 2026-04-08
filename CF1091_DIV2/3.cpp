4f677f8cc1f540e4c591e057a0f09ea949899311
SSHRIHARI006 <shrihari006projects@gmail.com>
2026-04-08 23:02:01 +0530

    commit

--- CF1091_DIV2/3.cpp
+++ CF1091_DIV2/3.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ld long double
-#define all(x) (x).begin(), (x).end()
-#define pb push_back
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -16,16 +11,9 @@ int main() {
         long long n, m, a, b;
         cin >> n >> m >> a >> b;
 
-        long long g1 = __gcd(a, n);
-        long long g2 = __gcd(b, m);
-
-        long long x = n / g1;
-        long long y = m / g2;
-
-        if (x % 2 == 0 && y % 2 == 0)
-            cout << "NO\n";
-        else
+        if (__gcd(n, a) == 1 && __gcd(m, b) == 1 && __gcd(n, m) <= 2)
             cout << "YES\n";
+        else
+            cout << "NO\n";
     }
-    return 0;
 }
